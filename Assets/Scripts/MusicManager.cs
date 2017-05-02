0c252630d1ea00c5ab7639e59b74d9bede8e4c12
Huayu <owlies.hwayu@gmail.com>
2017-05-01 22:52:50 -0700

    music player

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+   
+	// Use this for initialization
+	void Start () {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.PlayDelayed(4.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
